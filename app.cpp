0dbd0a60de9d933eb8633abb98ffc78189c9d185
granityo <frhnspensa@gmail.com>
2026-06-19 05:52:37 +0700

    using namespace std;

---
+++ app.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // file yang disertakan
+
+// using namespace std;
+using std::cout;
+using std::endl;
+
---
+++ keliling_persegi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // langkah 1
+    int luas, panjang, lebar;
+
+    // langkah 2
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+
+    // langkah 3
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+
+    // langkah 4
+    luas = panjang * lebar;
+
+    // langkah 5
+    cout << "Hasilnya : " << luas;
+    
+}
