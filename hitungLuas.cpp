7c1d2e8af2007e65c5207bcba36cc7fe9a25628f
granityo <frhnspensa@gmail.com>
2026-06-19 05:59:32 +0700

    menampilkan string

--- hitungLuas.cpp
+++ hitungLuas.cpp
@@ -9,3 +9,14 @@ int main() // mulai
     cout << "Masukkan panjang: ";
     cin >> p;
 
+    // langkah 3
+    cout << "Masukkan lebar: ";
+    cin >> l;
+
+    // langkah 4
+    luas = p * l;
+
+    // langkah 5
+    cout << "Hasilnya = " << luas;
+
+} // selesai
